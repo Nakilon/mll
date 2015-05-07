599c5d7691083870016f11ff34fe0fdf71257160
Nakilon <nakilon@gmail.com>
2015-05-07 10:50:52 +0300

    made universal checker if function should be applied to a List and deprecated method chaining

--- lib/mll.rb
+++ lib/mll.rb
@@ -1,33 +1,28 @@
-# http://reference.wolfram.com/language/guide/LanguageOverview.html
 module MLL
 
-  # http://reference.wolfram.com/language/guide/ListManipulation.html
-
-    # http://reference.wolfram.com/language/guide/ConstructingLists.html
-
-    # http://reference.wolfram.com/language/ref/Range.html
-    def self.range *args
-      # see also http://ruby-doc.org/core-1.9.3/Range.html#method-i-step
-      raise ArgumentError.new("wrong number of arguments (#{args.size} for 1..3)") unless (1..3).include? args.size
-      if args.size == 1
-        if args[0].respond_to?(:map)
-          args[0].map &method(:range)
+  def self.define_function_that_can_enumerate name, &block
+    # http://stackoverflow.com/a/12792313/322020
+    (class << self; self end).class_eval do
+      define_method name do |*args|
+        if args.size == 1 && args.first.respond_to?(:map)
+          args.first.map &method(name)
         else
-          range 1, args[0]
+          block.call *args
         end
-      else
-        Range.new(args[0], args[1]).step(args[2] || 1)
       end
     end
+  end
 
-    # http://reference.wolfram.com/language/guide/ElementsOfLists.html
-    # http://reference.wolfram.com/language/guide/RearrangingAndRestructuringLists.html
-    # http://reference.wolfram.com/language/guide/ApplyingFunctionsToLists.html
-    # http://reference.wolfram.com/language/guide/MathematicalAndCountingOperationsOnLists.html
-
-  # http://reference.wolfram.com/language/guide/FunctionalProgramming.html
+  define_function_that_can_enumerate :range do |*args|
+    raise ArgumentError.new("wrong number of arguments (#{args.size} for 1..3)") unless (1..3).include? args.size
+    next range 1, args[0] if args.size == 1
+    Range.new(args[0], args[1]).step(args[2] || 1)
+  end
 
 end
+
+
+=begin
 
       # http://reference.wolfram.com/language/guide/HandlingArraysOfData.html
       # http://reference.wolfram.com/language/guide/ComputationWithStructuredDatasets.html
@@ -68,3 +63,5 @@ class Fixnum
     # end
   end
 end
+
+=end
--- spec/_spec.rb
+++ spec/_spec.rb
@@ -1,36 +1,108 @@
 require_relative File.join "..", "lib", "mll"
 
 
+# http://reference.wolfram.com/language/guide/LanguageOverview.html
 describe MLL do
 
-  describe "range" do
+  # http://reference.wolfram.com/language/guide/ListManipulation.html
+  describe "List Manipulation" do
 
-    example "range(n)" do
-      expect(MLL::range(5)).to be_a Enumerator
-      expect(MLL::range(5).to_a).to eq [1,2,3,4,5]
+    # http://reference.wolfram.com/language/guide/ConstructingLists.html
+    describe "Constructing Lists" do
+
+      # http://reference.wolfram.com/language/ref/Range.html
+      describe "range" do
+
+        example "range( >3 args )" do
+          expect{ MLL::range(1,2,3,4) }.to raise_error ArgumentError
+        end
+
+        describe "Basic Examples" do
+
+          example "range(n)" do
+            expect(MLL::range(4)).to be_a Enumerator
+            expect(MLL::range(4).to_a).to eq [1,2,3,4]
+          end
+          example "range(imin, imax)" do
+            expect(MLL::range(2,5)).to be_a Enumerator
+            expect(MLL::range(2,5).to_a).to eq [2,3,4,5]
+          end
+          example "range(imin, imax, id)" do
+            expect(MLL::range(1,2,3)).to be_a Enumerator
+            expect(MLL::range(1,2,0.5).to_a).to eq [1,1.5,2]
+            expect(MLL::range(2,6,2).to_a).to eq [2,4,6]
+            expect(MLL::range(-4,9,3).to_a).to eq [-4,-1,2,5,8]
+          end
+          # example "range(imin, imax, id)" do
+          #   expect(MLL::range(2,5,2)).to be_a Enumerator
+          #   expect(MLL::range(2,5,2).to_a).to eq [2,4]
+          #   expect(MLL::range(2,6,2).to_a).to eq [2,4,6]
+          # end
+        end
+
+        describe "Generalizations & Extensions" do
+
+          example "range([n1, n2, n3, n4])" do
+            # expect(MLL::range([2,3])).to be_a Enumerator # TODO revive me
+            MLL::range([5,2,6,3]).each do |i|
+              expect(i).to be_a Enumerator
+            end
+            expect(MLL::range([5,2,6,3]).map(&:to_a)).to eq [[1,2,3,4,5],[1,2],[1,2,3,4,5,6],[1,2,3]]
+          end
+
+        end
+
+        describe "Applications" do
+
+          example "power(2, range(5))" do
+            pending "#power is yet to be implemented"
+            p power(2, range(5))
+            expect(power(2, range(5))).to eq [2,4,8,16,32]
+          end
+
+        end
+
+        describe "Neat Examples" do
+
+          example "range(range(n))" do
+            # expect(MLL::range(MLL::range(3))).to be_a Enumerator # TODO revive me
+            MLL::range(MLL::range(5)).each do |i|
+              expect(i).to be_a Enumerator
+            end
+            expect(MLL::range(MLL::range(5)).map(&:to_a)).to eq [[1],[1,2],[1,2,3],[1,2,3,4],[1,2,3,4,5]]
+          end
+          example "range(range(range(n)))" do
+            # expect(MLL::range(MLL::range(3))).to be_a Enumerator # TODO revive me
+            # MLL::range(MLL::range(MLL::range(3))).each do |i|
+            #   expect(i).to be_a Enumerator
+            # end
+            expect(MLL::range(MLL::range(MLL::range(3))).map{ |i| i.map(&:to_a) }).to eq [[[1]],[[1],[1,2]],[[1],[1,2],[1,2,3]]]
+          end
+
+          # TODO Nest[Range,3,6]
+
+        end
+
+        # TODO "Find an inverse permutation:"
+        # TODO "Use Range or Span (;;) as Part specification:"
+
+      end
+
     end
-    example "range(imin, imax)" do
-      expect(MLL::range(2,5)).to be_a Enumerator
-      expect(MLL::range(2,5).to_a).to eq [2,3,4,5]
-    end
-    example "range(imin, imax, id)" do
-      expect(MLL::range(2,5,2)).to be_a Enumerator
-      expect(MLL::range(2,5,2).to_a).to eq [2,4]
-      expect(MLL::range(2,6,2).to_a).to eq [2,4,6]
-    end
-    example "range([n1,n2])" do
-      # TODO add .to be_a Enumerator
-      expect(MLL::range([2,3]).map(&:to_a)).to eq [[1,2],[1,2,3]]
-    end
-    example "range(range(n))" do
-      # TODO add .to be_a Enumerator
-      expect(MLL::range(MLL::range(3)).map(&:to_a)).to eq [[1],[1,2],[1,2,3]]
-    end
+
+    # http://reference.wolfram.com/language/guide/ElementsOfLists.html
+    # http://reference.wolfram.com/language/guide/RearrangingAndRestructuringLists.html
+    # http://reference.wolfram.com/language/guide/ApplyingFunctionsToLists.html
+    # http://reference.wolfram.com/language/guide/MathematicalAndCountingOperationsOnLists.html
 
   end
 
+  # http://reference.wolfram.com/language/guide/FunctionalProgramming.html
+
 end
 
+
+=begin
 
 describe "range" do
 
@@ -66,3 +138,5 @@ describe "range" do
   # TODO 4 args should fail
 
 end
+
+=end
